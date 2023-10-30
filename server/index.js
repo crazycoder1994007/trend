8f83ff098770d51456c3f8c2af3c3a4d77e2be70
certifieddev0101 <wildhorse0916@skiff.com>
2023-10-30 10:21:38 -0400

    Initial Setup project

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
 // Server - running on Node.js
 
-import 'dotenv/config';
-import mongoose from 'mongoose';
-import routes from './routes/router';
-import googleTrends from './data/google';
-import redditSubs from './data/reddit';
-import youtubeVideos from './data/youtube';
-import twitterTrends from './data/twitter';
+import "dotenv/config";
+import mongoose from "mongoose";
+import routes from "./routes/router";
+import googleTrends from "./data/google";
+import redditSubs from "./data/reddit";
+import youtubeVideos from "./data/youtube";
+import twitterTrends from "./data/twitter";
 
 routes.init();
 
-mongoose.connect('mongodb://localhost/trends', { useNewUrlParser: true });
+mongoose.connect(
+  "mongodb+srv://trend2023:enjoymylife0101@cluster0.mko4g5w.mongodb.net/",
+  { useNewUrlParser: true }
+);
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, '[trends server] MongoDB error:'));
-db.once('open', () => {
-    console.log('[trends server] Successfully connected to MongoDB!');
-    redditSubs.getSubs();
-    googleTrends.getTrends();
-    youtubeVideos.getVideos();
-    twitterTrends.getTrends();
+db.on("error", console.error.bind(console, "[trends server] MongoDB error:"));
+db.once("open", () => {
+  console.log("[trends server] Successfully connected to MongoDB!");
+  //   redditSubs.getSubs();
+  googleTrends.getTrends();
+  //   youtubeVideos.getVideos();
+  //   twitterTrends.getTrends();
 });
